e2e9e83948c22bb04c00ecafb12a215abeb97af2
Alexandre Roulois <alexandre.roulois@linguist.univ-paris-diderot.fr>
2019-11-05 13:22:27 +0100

    Feature: printing syllables of a word

--- main.js
+++ main.js
@@ -1,2 +1,13 @@
 // Main.js
-
+var app = new Vue({
+    el: '#app',
+    data: {
+        word: 'aéroport',
+        syllables: ['a','é','ro','port'],
+    },
+    computed: {
+        nbSyllables: function() {
+            return this.syllables.length;
+        }
+    }
+})
