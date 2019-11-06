629abf5f0d8eaf9f56ef1f4915d03aca9327711e
Alexandre Roulois <alexandre.roulois@linguist.univ-paris-diderot.fr>
2019-11-06 11:43:12 +0100

    Feature: color brick if correctly reordered

---
+++ components/bricks.js
@@ -0,0 +1,46 @@
+// bricks.js
+let bricksComponent = {
+    props: {
+        syllables: {
+            type: Array,
+            required: true
+        }
+    },
+    template: `
+        <div class="col-md-4">
+            <h2>Syllabes ({{ nbSyllables }})</h2>
+            <ul class="list-inline">
+                <li
+                    v-for="syllable in syllables"
+                    class="list-inline-item">
+                    <button
+                        :key="syllable.order"
+                        @click="addToResetZone(syllable)"
+                        role="button"
+                        class="btn btn-light"
+                        :disabled="!syllable.isDisplayed">
+                        {{ syllable.text }}
+                    </button>
+                </li>
+            </ul>
+        </div>
+    `,
+    methods: {
+        /*
+        *   Emits an event called add-to-reset-zone
+        *   @param {String} syllable: the syllable to add to the reset zone
+        */
+        addToResetZone: function(syllable) {
+            syllable.isDisplayed = false;
+            this.$emit('add-to-reset-zone', syllable.text, syllable.order);
+        }
+    },
+    computed: {
+        /*
+        *   Counts the number of syllables
+        */
+        nbSyllables: function() {
+            return this.syllables.length;
+        }
+    }
+}
--- main.js
+++ main.js
@@ -2,20 +2,58 @@
 let app = new Vue({
     el: '#app',
     data: {
-        word: 'a-é-ro-port',
-        syllables: []
+        word: 'aéroport',
+        syllables: [
+            {
+                order: 0,
+                text: "a",
+                isDisplayed: true
+            },
+            {
+                order: 3,
+                text: "port",
+                isDisplayed: true
+            },
+            {
+                order: 2,
+                text: "ro",
+                isDisplayed: true
+            },
+            {
+                order: 1,
+                text: "é",
+                isDisplayed: true
+            }
+        ],
+        bricks: []
     },
     components: {
-        'syllables': syllablesComponent,
+        'bricks': bricksComponent,
         'reset': resetComponent
     },
     methods: {
         /*
+        *   Shuffles array in place.
+        *   Fisher-Yates modern shuffle algorithm
+        *   @param {Array} a: array containing the items.
+        */
+        mixItems: function(a) {
+            let j, x, i;
+            for (i = a.length - 1; i > 0; i--) {
+                j = Math.floor(Math.random() * (i + 1));
+                x = a[i];
+                a[i] = a[j];
+                a[j] = x;
+            }
+            return a;
+        },
+        /*
         *   Updates the reset zone of the word
         *   @param {String} syllable: the syllable to reveal
+        *   @param {Number} order: the correct order of the syllable in the word
         */
-        updateResetZone: function(syllable) {
-            this.syllables.push(syllable);
+        updateResetZone: function(syllable, order) {
+            this.bricks.push({ syllable: syllable, order: order });
         }
     }
 })
