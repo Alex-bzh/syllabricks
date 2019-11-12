de253dbf6207ce76a2a3ebca9dbc14eb9c554c1a
Alexandre Roulois <alexandre.roulois@linguist.univ-paris-diderot.fr>
2019-11-12 13:16:10 +0100

    Fix: record a word into its lemm form

--- scripts/lex3ToJSON.py
+++ scripts/lex3ToJSON.py
@@ -2,7 +2,7 @@
 #-*- coding: utf-8 -*-
 
 #
-#	Modules to import
+#   Modules to import
 #
 import csv, json
 
@@ -11,42 +11,43 @@ import csv, json
 #
 if __name__ == "__main__":
 
-	# The lexicon to compose
-	lexicon = {
-		"words": []
-	}
-	# We want to keep track of the lemmas
-	lemmas = set()
+    # The lexicon to compose
+    lexicon = {
+        "words": []
+    }
+    # We want to keep track of the lemmas
+    lemmas = set()
 
-	# Link to Lexique3 (not included with the app)
-	Lexique3 = '../assets/Lexique383/Lexique383.tsv'
+    # Link to Lexique3 (not included with the app)
+    Lexique3 = '../assets/Lexique383/Lexique383.tsv'
 
-	# Working with the tabulated version of Lexique v3.8.3
-	with open(Lexique3, newline='') as file:
-		# All the lines of the file
-		lines = csv.reader(file, delimiter='\t')
-		# For each line in the file…
-		for line in lines:
-			# Fast access to the lemma
-			lemma = line[2]
-			# We don't keep the words that have only one syllable,
-			# neither than the words which lemme is already recorded
-			if len(line[22].split('-')) > 1 and lemma not in(lemmas):
-				# … the lexicon is updated with a simple dictionary
-				lexicon["words"].append(
-					{
-						'word': line[2],
-						'syll': line[22],
-						'orthosyll': line[27]
-					}
-				)
-				# Record the lemme
-				lemmas.add(lemma)
+    # Working with the tabulated version of Lexique v3.8.3
+    with open(Lexique3, newline='') as file:
+        # All the lines of the file
+        lines = csv.reader(file, delimiter='\t')
+        # For each line in the file…
+        for line in lines:
+            # Fast access to the word, its lemma, ortho and phonetic representations
+            word, lemma, syll, orthosyll = line[0], line[2], line[22], line[27]
+            # We don't keep the words that have only one syllable,
+            # neither than the words which lemma is already recorded.
+            # Lastly, only record the word under its lemma form.
+            if len(syll.split('-')) > 1 and lemma not in lemmas and lemma == word:
+                # Record the lemma
+                lemmas.add(lemma)
+                # … the lexicon is updated with a simple dictionary
+                lexicon["words"].append(
+                    {
+                        'word': word,
+                        'syll': syll,
+                        'orthosyll': orthosyll
+                    }
+                )
 
-	# Deleting the first line (headers)
-	lexicon["words"] = lexicon["words"][1:]
+    # Deleting the first line (headers)
+    lexicon["words"] = lexicon["words"][1:]
 
-	# Writing a JSON dump of the lexicon in the store
-	with open('../store/lexicon.json', 'w') as dest:
-		# Be sure to keep the utf-8 format with ensure_ascii=false
-		dest.write(json.dumps(lexicon, ensure_ascii=False, indent=2))
+    # Writing a JSON dump of the lexicon in the store
+    with open('../store/lexicon.json', 'w') as dest:
+        # Be sure to keep the utf-8 format with ensure_ascii=false
+        dest.write(json.dumps(lexicon, ensure_ascii=False, indent=2))
